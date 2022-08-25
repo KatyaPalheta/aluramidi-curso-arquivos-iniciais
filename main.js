8ed4ae1efaa6179361934718ceed40c3070fa748
KATYA SYLENE FONTENELLE PALHETA <109077065+KatyaPalheta@users.noreply.github.com>
2022-08-25 01:46:49 -0300

    finalizado
    finalizado como o projeto original

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-let tecla = document.querySelectorAll('[data-botao]');
-let som = document.querySelectorAll('[data-som]');
+/*
+function tocaSom(idElementoAudio) {
+	document.querySelector(idElementoAudio).play();
+}
 
-tecla.forEach((element) => {
-	element.addEventListener('click', (evento) => {
-		tocaSom(evento.target.dataset);
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+	const tecla = listaDeTeclas[contador];
+	const instrumento = tecla.classList[1];
+	const idAudio = `#som_${instrumento}`; //template string
+
+	tecla.onclick = function () {
+		tocaSom(idAudio);
+	};
+}
+*/
+
+const botao = document.querySelectorAll('[data-botao]');
+const som = document.querySelectorAll('[data-som]');
+
+botao.forEach((element) => {
+	element.addEventListener('click', (event) => {
+		tocaSom(event.target.dataset);
 	});
 });
+function tocaSom(botao) {
+	if (botao == som.dataset) {
+		som.play();
+	}
+}
